2a4cce015ad48eb105b8f40c49c47f93bdee4486
Chibuzo <daunikman@gmail.com>
2022-07-22 00:28:20 +0100

    Changed folder structure

---
+++ TechnicalAssessmentApp/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path, include
+from . import views
+from TechnicalAssessmentApp import views
+
+urlpatterns = [
+    path('', views.list_of_books, name='list_of_books'),
+    path('post', views.books_form, name='book_register'),
+    path('delete_book/<int:id>/', views.books_delete, name='book_delete'),
+    path('list/', views.list_of_books, name='list_of_books'),
+    path('update/<int:id>/', views.books_form, name='book_update'),
+    path('retrieve_book_details/<int:id>', views.retrieve_project, name="retrieve_project"),
+
+    path('aut', views.author_form, name='author_register'),
+    path('<int:id>/', views.author_form, name='author_update'),
+    path('delete/<int:id>/', views.author_delete, name='author_delete'),
+    path('authorlist/', views.list_of_authors, name='list_of_authors'),
+
+]
---
+++ TechnicalAssessmentApp/views.py
@@ -0,0 +1,73 @@
+from django.shortcuts import render, redirect
+from TechnicalAssessmentApp.forms import Booksform
+from TechnicalAssessmentApp.authorForms import AuthorForm
+
+from .models import Book, Author
+
+
+def list_of_books(request):
+    context = {'list_of_books': Book.objects.all()}
+    return render(request, "List.html", context)
+
+
+def books_form(request, id=0):
+    if request.method == "GET":
+        if id == 0:
+            form = Booksform()
+        else:
+            book = Book.objects.get(pk=id)
+            form = Booksform(instance=book)
+        return render(request, "Register.html", {'form': form})
+    else:
+        if id == 0:
+            form = Booksform(request.POST)
+        else:
+            book = Book.objects.get(pk=id)
+            form = Booksform(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('/books/list')
+
+
+def books_delete(request, id):
+    book = Book.objects.get(pk=id)
+    book.delete()
+    return redirect('/books/list')
+
+
+def list_of_authors(request):
+    context = {'list_of_authors': Author.objects.all()}
+    return render(request, "AuthorList.html", context)
+
+
+def author_form(request, id=0):
+    if request.method == "GET":
+        if id == 0:
+            authorForm = AuthorForm()
+        else:
+            author = Author.objects.get(pk=id)
+            authorForm = AuthorForm(instance=author)
+        return render(request, "AuthorRegister.html", {'form': authorForm})
+    else:
+        if id == 0:
+            authorForm = AuthorForm(request.POST)
+        else:
+            author = Author.objects.get(pk=id)
+            authorForm = AuthorForm(request.POST, instance=author)
+        if authorForm.is_valid():
+            authorForm.save()
+            return redirect('/authors/authorlist')
+
+
+def author_delete(request, id):
+    author = Author.objects.get(pk=id)
+    author.delete()
+    return redirect('/authors/authorlist')
+
+
+def retrieve_project(request, id):
+    if Book.objects.filter(pk=id).exists():
+        project = Book.objects.get(pk=id)
+    else:
+        project = {}
+    return render(request, "Book_details.html", {'project': project})
