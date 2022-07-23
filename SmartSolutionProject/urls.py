3b11eddca02e6ac3288d4a860ca363be9315f195
Chibuzo <daunikman@gmail.com>
2022-07-23 13:56:40 +0100

    Done with routing

--- SmartSolutionProject/urls.py
+++ SmartSolutionProject/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('TechnicalAssessmentApp.urls')),
     path('books/', include('TechnicalAssessmentApp.urls')),
     path('authors/', include('TechnicalAssessmentApp.urls')),
 ]
